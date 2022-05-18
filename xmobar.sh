1cb1ab482d18682f753c1a54496ac61c782b190e
hackartists <js.choi.85@gmail.com>
2022-05-19 00:01:16 +0900

    compact menubar

--- xmobar.sh
+++ xmobar.sh
@@ -3,4 +3,4 @@
 export XMOBAR_FONT=xft:NanumGothic:size=9:bold:antialias=true
 export XMONAD_TRAY_MONITOR=0
 
-xmobar -x 0 --font=$XMOBAR_FONT --position='Static {xpos=0,ypos=0,width=1920,height=25}' $HOME/.xmonad/xmobarrc.hs
+xmobar -x 0 --font=$XMOBAR_FONT --position='Static {xpos=0,ypos=0,width=2880,height=25}' $HOME/.xmonad/xmobarrc.hs
--- xmobarrc.hs
+++ xmobarrc.hs
@@ -14,16 +14,24 @@ Config {
        , persistent = True
        , iconRoot = "~/.xmonad/xpm/"  -- default: "."
        , commands = [
-                    -- Time and date
-                      Run Date "%b %d %Y - (%H:%M) " "date" 50
-                      -- Network up and down
+                      Run Date "%y/%m/%d %H:%M" "date" 50
+                    , Run Battery [
+	                "-t",
+                        "<left>%-<timeleft>",
+	                "--",
+	                --"-c", "charge_full",
+	                "-O", "AC",
+	                "-o", "Bat",
+	                "-h", "green",
+	                "-l", "red"
+	                ] 10
                     , Run Network "eth0" ["-t", "<rx>kb  <fn=2>\xf0aa</fn>  <tx>kb"] 20
                       -- Cpu usage in percent
                     , Run Cpu ["-t", "<total>%","-H","50","--high","red"] 20
                       -- Ram used number and percent
                     , Run Memory ["-t", "<used>M"] 20
                       -- Disk space free
-                    , Run DiskU [("/", "<free> free")] [] 60
+                    , Run DiskU [("/", "<free>")] [] 60
                       -- Runs custom script to check for pacman updates.
                       -- This script is in my dotfiles repo in .local/bin.
                     , Run Com "bash" ["-c", "~/.local/bin/pacupdate"] "updates" 3600
@@ -40,5 +48,5 @@ Config {
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = " <icon=haskell_20.xpm/>   <fc=#666666>|</fc> %UnsafeStdinReader% }{ <fc=#666666>|</fc>   <fc=#c678dd>%mouselocation%</fc> <fc=#666666>|</fc>  <fc=#b3afc2> <action=`xdg-open https://github.com/notifications`>GH-%gh%</action> </fc> <fc=#666666>|</fc>  <fc=#b3afc2> <action=`alacritty -e htop`>%uname%</action> </fc> <fc=#666666>|</fc>  <fc=#ecbe7b> <action=`alacritty -e htop`>%cpu%</action> </fc> <fc=#666666>|</fc>  <fc=#ff6c6b> <action=`alacritty -e htop`>%memory%</action> </fc> <fc=#666666>|</fc>  <fc=#51afef> <action=`alacritty -e htop`>%disku%</action> </fc> <fc=#666666>|</fc>   <fc=#c678dd> <action=`alacritty -e sudo pacman -Syu`>%updates%</action> </fc> <fc=#666666>|</fc>  <fc=#46d9ff> <action=`emacsclient -n --eval '(cfw:open-org-calendar)'`>%date%</action> </fc><fc=#666666><fn=1>|</fn></fc>"
+       , template = " <icon=haskell_20.xpm/>   <fc=#666666>|</fc> %UnsafeStdinReader% }{ <fc=#666666>|</fc>   <fc=#c678dd>%mouselocation%</fc> <fc=#666666>|</fc>  <fc=#b3afc2> %battery% </fc> <fc=#666666>|</fc>   <fc=#b3afc2> <action=`xdg-open https://github.com/notifications`>G%gh%</action> </fc> <fc=#666666>|</fc>  <fc=#b3afc2> <action=`alacritty -e htop`>%uname%</action> </fc> <fc=#666666>|</fc>  <fc=#ecbe7b> <action=`alacritty -e htop`>%cpu%</action> </fc> <fc=#666666>|</fc>  <fc=#ff6c6b> <action=`alacritty -e htop`>%memory%</action> </fc> <fc=#666666>|</fc>  <fc=#51afef> <action=`alacritty -e htop`>%disku%</action> </fc> <fc=#666666>|</fc>   <fc=#c678dd> <action=`alacritty -e sudo pacman -Syu`>%updates%</action> </fc> <fc=#666666>|</fc>  <fc=#46d9ff> <action=`emacsclient -n --eval '(cfw:open-org-calendar)'`>%date%</action> </fc><fc=#666666><fn=1>|</fn></fc>"
        }
