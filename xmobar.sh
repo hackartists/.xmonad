93d3142aaaa200b0ec1b8250c225a530c3bbc611
hackartists <js.choi.85@gmail.com>
2022-05-21 06:45:08 +0900

    apply env

--- xmobar.sh
+++ xmobar.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-export XMOBAR_FONT=xft:NanumGothic:size=9:bold:antialias=true
-export XMONAD_TRAY_MONITOR=0
+source $HOME/.xmonad/.env
 
-xmobar -x 0 --font=$XMOBAR_FONT --position='Static {xpos=0,ypos=0,width=2880,height=25}' $HOME/.xmonad/xmobarrc.hs
+xmobar -x $XMONAD_TRAY_MONITOR --font=$XMOBAR_FONT --position="Static {xpos=$XMOBAR_POS,ypos=0,width=$XMOBAR_WIDTH,height=25}" $HOME/.xmonad/xmobarrc.hs
