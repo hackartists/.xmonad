ddc5b8de1d0d93aee32f7b98df6de02302e068e0
hackartists <js.choi.85@gmail.com>
2022-06-06 13:40:09 +0900

    add mongodb compass manage hook

--- tray.sh
+++ tray.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-export XMONAD_TRAY_MONITOR=0
+source $HOME/.xmonad/.env
 
 trayer --edge top --align center --widthtype request --padding 0 --SetDockType true --SetPartialStrut true --expand true --monitor $XMONAD_TRAY_MONITOR --transparent true --alpha 0 --tint 0x282c34  --height 22 --iconspacing 5
--- xmonad.hs
+++ xmonad.hs
@@ -628,6 +628,7 @@ myManageHook = composeAll
      , className =? "obs"                          --> doShift "8:media"
      , className =? "kdenlive"                     --> doShift "8:media"
      , className =? "SimpleScreenRecorder"         --> doShift "8:media"
+     , resource =? "MongoDB Compass"   --> doShift "9:database"
      , resource =? "mysql-workbench-bin"          --> doShift "9:database"
      , title =? "NoiseTorch" --> doShift "0:misc"
      , className =? "scrcpy"                       --> (doShift "0:misc" <+> doFloat)
