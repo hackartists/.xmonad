ed5a5003d41fe7fa2847e78e6fcc84418001f46b
hackartists <js.choi.85@gmail.com>
2022-06-27 18:04:13 +0900

    xmobar y position

--- xmobar.sh
+++ xmobar.sh
@@ -2,4 +2,5 @@
 
 source $HOME/.xmonad/.env
 
+echo "xmobar -x $XMONAD_TRAY_MONITOR --font=$XMOBAR_FONT --position=\"Static {xpos=$XMOBAR_POS,ypos=$XMOBAR_YPOS,width=$XMOBAR_WIDTH,height=25}\" $HOME/.xmonad/xmobarrc.hs"
 xmobar -x $XMONAD_TRAY_MONITOR --font=$XMOBAR_FONT --position="Static {xpos=$XMOBAR_POS,ypos=0,width=$XMOBAR_WIDTH,height=25}" $HOME/.xmonad/xmobarrc.hs
