c9bc59c1760587864049ca651eb4134c73616f28
hackartists <js.choi.85@gmail.com>
2022-08-27 08:59:57 +0900

    add tray height variable

--- tray.sh
+++ tray.sh
@@ -2,4 +2,4 @@
 
 source $HOME/.xmonad/.env
 
-trayer --edge top --align center --widthtype request --padding 0 --SetDockType true --SetPartialStrut true --expand true --monitor $XMONAD_TRAY_MONITOR --transparent true --alpha 0 --tint 0x282c34  --height 22 --iconspacing 5
+trayer --edge top --align center --widthtype request --padding 0 --SetDockType true --SetPartialStrut true --expand true --monitor $XMONAD_TRAY_MONITOR --transparent true --alpha 0 --tint 0x282c34  --height $XMONAD_TRAY_HEIGHT --iconspacing 5
