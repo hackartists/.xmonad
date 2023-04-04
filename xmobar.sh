3839c1c6ce8d597753ef59018878b5bcfdc3667d
hackartists <hackartists@gmail.com>
2023-04-04 19:49:40 +0900

    xmonad width

--- xmobar.sh
+++ xmobar.sh
@@ -2,4 +2,9 @@
 
 source $HOME/.xmonad/.env
 
+if [ "$XMOBAR_WIDTH" == "" ]
+then
+    XMOBAR_WIDTH=`xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/' | sed -r 's/x.*//'`
+fi
+
 xmobar -x $XMONAD_TRAY_MONITOR --font=$XMOBAR_FONT --position="Static {xpos=$XMOBAR_POS,ypos=$XMOBAR_YPOS,width=$XMOBAR_WIDTH,height=25}" $HOME/.xmonad/xmobarrc.hs
