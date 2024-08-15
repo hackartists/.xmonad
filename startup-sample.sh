2a0d4a8a924f5ff3a040cdc00daba76648621f73
hackartists <hackartists@gmail.com>
2024-08-15 12:46:51 +0900

    change start-up.sh

--- startup-sample.sh
+++ startup-sample.sh
@@ -1,17 +1,17 @@
-
-
 echo "Starting custom programs"
 export GTK_IM_MODULE='ibus'
 export QT_IM_MODULE='ibus'
 export XMODIFIERS='@im=ibus'
+export GLFW_IM_MODULE=ibus
+export XDG_SESSION_TYPE=x11
 
 # lxsession &
 # setxkbmap dvorak &
 # conky -c $HOME/.config/conky/xmonad.conkyrc
 
 # xset r rate 150 30
-# xset r rate 150 50
-xset r rate 200 30
+xset r rate 150 50
+# xset r rate 200 15
 xset s off -dpms
 # setterm -blank 0 -powerdown 0
 setxkbmap -option caps:escape
@@ -27,8 +27,8 @@ uim-xim&
 picom &
 nm-applet &
 pa-applet &
-# blueman-manager &
-# blueman-applet &
+blueman-manager &
+blueman-applet &
 volumeicon &
 greenclip daemon &
 
@@ -36,43 +36,36 @@ greenclip daemon &
 copyq &
 
 google-chrome-stable &
-google-chrome-unstable --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36" https://www.figma.com/ &
+# google-chrome-unstable --user-agent="Mozilla/5.0 (Windows NT6.4; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36" https://www.figma.com/ &
 google-chrome-beta &
 
 emacs --name emacs-main &
 
-slack &
-chatall &
+slack -u&
 
 function im-uim {
     QT_IM_MODULE='uim' GTK_IM_MODULE='uim' XMODIFIERS='@im=uim' $@
 }
 
-im-uim whatsdesk --force-device-scale-factor=1.5 &
-telegram-desktop &
+QT_IM_MODULE='uim' GTK_IM_MODULE='uim' XMODIFIERS='@im=uim' im-uim whatsdesk & ## --force-device-scale-factor=1.5 &
+# telegram-desktop --no-startup-window &
+discord --start-minimized &
 
-discord &
-# yakyak&
-# zoom&
-# /opt/notifier/bin/notifier.AppImage&
 # autokey-gtk &
 
 mkdir -p ~/data/google-drive/biyard ~/data/google-drive/bylabs ~/data/google-drive/hackartist
-function google_mount {
+function mount_cloud_storage {
     account=$1
     dir=$2
 
     while true
     do
-        rclone mount "$account:/" "$dir" --vfs-cache-mode full --vfs-cache-max-size=500G --vfs-cache-max-age=3600h --vfs-refresh --vfs-fast-fingerprint --drive-server-side-across-configs
+        rclone mount "$account:/" "$dir" --dir-cache-time 1d --vfs-cache-max-age 3600h --vfs-cache-max-size 500G --vfs-refresh --vfs-cache-mode full --drive-server-side-across-configs
     done
 }
-google_mount biyard ~/data/google-drive/biyard &
-google_mount biyard-admin ~/data/google-drive/biyard-admin &
-google_mount biyard-madapp ~/data/google-drive/bylabs &
-google_mount hackartist ~/data/google-drive/hackartist &
+mount_cloud_storage config-name $HOME/data/google-drive/dir-name &
 
-function google_sync {
+function sync_cloud_storage {
     remote=$1
     local="$HOME/data/$2"
     # dir="$HOME/data/$(echo $remote | tr ":" "/")"
@@ -81,15 +74,26 @@ function google_sync {
     while true
     do
         rclone -v bisync "$remote" "${local}" --drive-server-side-across-configs --resync # --drive-allow-import-name-change 
-        # rclone -v bisync "$remote" "${local}" --exclude=/참고자료/** --drive-allow-import-name-change --resync 
         sleep 300
     done
 }
+sync_cloud_storage "config-name:Cloud Dirname" "dir-name" &
 
-google_sync "bylabs:Design sources" "design-sources" &
-# google_sync "biyard:Admin & Compliance" "biyard/admin" &
-onedrive -m --syncdir ~/data/onedrive/hackartist --disable-notifications --display-sync-status&
+# ollama serve &
+
+function pull_home {
+    cd ~/.emacs_anywhere && git pull && cd ..  &
+    cd ~/.oh-my-profiles && git pull && cd .. &
+    cd ~/.xmonad && git pull && cd .. &
+    cd ~/.config && git pull && cd .. &
+    cd ~/.gnupg && git pull && cd .. &
+    cd ~/.emacs.d && git pull && cd .. &
+    cd ~/.password-store && git pull && cd .. &
+    cd ~/data/devel/github.com/hackartists/notes/ && git pull && cd ~ &
+}
+# pull_home &
+
 # sshfs -o default_permissions server:/home/hackartist ~/data/server &
-# wine "/home/hackartist/.wine/drive_c/Program Files (x86)/Kakao/KakaoTalk/KakaoTalk.exe"
-vmware &
-yay -Syyu --noconfirm
+
+PYTHONPATH=$HOME/.xmonad/py python -m ranger-dbus.py &
+/usr/lib/xdg-desktop-portal-termfilechooser -c $HOME/.config/xdg-desktop-portal-termfilechooser/config -l DEBUG &
